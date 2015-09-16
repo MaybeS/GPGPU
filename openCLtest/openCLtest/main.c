5ea72684923fad9d298e2edbefb5fbe6f1597208
Maybe <maybe@yonsei.ac.kr>
2015-09-17 02:13:57 +0900

    clBuildProgram err-11,
    왜이러는지모르겠다 ㅁㄴㅇㄹ

--- openCLtest/openCLtest/main.c
+++ openCLtest/openCLtest/main.c
@@ -21,6 +21,24 @@
 #define BUFFER_SIZE (256)
 #define MAX_SOURCE_SIZE (0x100000)
 
+char * file_read(const char *, size_t *);
+
+
+char * file_read(const char * path, size_t * size)
+{
+	FILE * file = fopen(path, "rb");
+	char * file_text;
+
+	fseek(file, 1, SEEK_END);
+	(*size) = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	file_text = (char*)malloc(sizeof(char) * (*size));
+	fread(file_text, sizeof(char), (*size), file);
+	fclose(file);
+
+	return file_text;
+}
 int main(int argc, char * argv[])
 {
 	cl_int err = NO_ERR, size;
@@ -28,7 +46,9 @@ int main(int argc, char * argv[])
 	cl_device_id * devices;
 	cl_uint platfor_num, device_num;
 
-	char buffer[BUFFER_SIZE];
+	size_t _index, _index__, _index___;
+
+	char buffer[BUFFER_SIZE], _buffer[BUFFER_SIZE];
 
 	err += clGetPlatformIDs(1, &platform, &platfor_num);
 	printf("openCL Platform detected: %d\n", platfor_num);
@@ -42,16 +62,52 @@ int main(int argc, char * argv[])
 	err += clGetPlatformInfo(platform, CL_PLATFORM_VERSION, BUFFER_SIZE, buffer, &size);
 	printf("Platform version: %s\n", buffer);
 
-	err += clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, 0, &device_num);
+	err += clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 0, 0, &device_num);
 	printf("Devices detected: %d\n", device_num);
 
 	devices = (cl_device_id *)malloc(sizeof(cl_device_id) * device_num);
-	err += clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, device_num, devices, &device_num);
-	for (int device_index = 0; device_index < device_num; device_index++)
+	err += clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, device_num, devices, &device_num);
+	for (_index = 0; _index < device_num; _index++)
 	{
-		err += clGetDeviceInfo(devices[device_index], CL_DEVICE_NAME, 512, buffer, &size);
-		printf("Device%d name: %s\n", device_index, buffer);
+		err += clGetDeviceInfo(devices[_index], CL_DEVICE_NAME, BUFFER_SIZE, buffer, &size);
+		printf("Device%d name: %s\n", _index, buffer);
 	}
+
+	if (err)
+		return -1;
+
+	cl_context context;
+	cl_context_properties context_properties[] = { CL_CONTEXT_PLATFORM, platform, 0 };
+	context = clCreateContext(context_properties, device_num, devices, NULL, NULL, &err);
+
+	for (_index = 0; _index < device_num; _index++)
+	{
+		char device_name[BUFFER_SIZE];
+		err += clGetDeviceInfo(devices[_index], CL_DEVICE_NAME, BUFFER_SIZE, device_name, &size);
+		err += clGetDeviceInfo(devices[_index], CL_DEVICE_COMPILER_AVAILABLE, BUFFER_SIZE, buffer, (size_t*)&size);
+		if(buffer[0] == CL_TRUE)
+			printf("%s support kernel compile!\n", device_name);
+		else
+			printf("%s does not support kernel compile!\n", device_name);
+	}
+
+	if (err)
+		return -1;
+
+	char * kernel_source = file_read("./test.cl", &size);
+	printf("%s", kernel_source);
+	cl_program program = clCreateProgramWithSource(context, 1, &kernel_source, &size, &err);
+	err += clBuildProgram(program, device_num, devices, NULL, NULL, NULL);
+
+	cl_kernel kernel = clCreateKernel(program, "hello", &err);
+
+	cl_mem memory = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR, BUFFER_SIZE, buffer, &size);
+	err += clSetKernelArg(kernel, 0, sizeof(cl_mem), &memory);
+
+	cl_command_queue queue = clCreateCommandQueue(context, *devices, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, &err);
+
+	err += clEnqueueNDRangeKernel(queue, kernel, 1, 0, &size, 0, 0, 0, 0);
+	err = clFinish(queue);
 
 
 	return 0;
